c4bff7d2e7338a4e0a4b034d27d6d57763cceb2b
magnetar-star <matthew.lvk@gmail.com>
2023-05-13 15:47:10 +0200

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+def fizzbuzz(max_num: int):
+    m = 'buzz'
+    res = []
+    for n in range(1, max_num+1, 1):
+        if n % 3 == 0 and m == 'fizz':
+            res.append('buzz')
+            m = 'buzz'
+        elif n % 3 == 0 and m == 'buzz':
+            res.append('fizz')
+            m = 'fizz'
+        else:
+            res.append(n)
+
+    print(res)
+
+if __name__ == '__main__':
+    print('Enter number you want for fizz-buzz: ')
+    number = input()
+    fizzbuzz(number)
